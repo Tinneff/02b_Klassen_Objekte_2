cb5eefedc6622de6d38077804777268c845d13e5
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-02-01 11:40:04 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,32 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        Cat cat = new Cat("Grizabella", "white", 29);
+
+        output(cat.getName());
+        output(cat.getFurColor());
+        output(String.valueOf(cat.getAge()));  
+
+
+        cat.setFurColor("grey");
+        output(cat.getFurColor());
+
+        output("-------------------------");
+
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
+
+        output(cat2.getName());
+        output(cat2.getFurColor());
+        output(String.valueOf(cat2.getAge())); 
+
+    }
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+}
+
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,49 @@
+package com.cc.java;
+
+public class Cat {
+
+  private String name;
+  private String furColor;
+  private int age; 
+
+public Cat(String name, String furColor, int age) {
+    this.name = name;
+    this.furColor = furColor;
+    this.age = age;
+  }
+
+public String getName() {
+  if (checkPermission()) {
+    return name;
+  } else {
+    return "Sorry, no permission!";
+  }
+}
+
+public void setName(String name) {
+  this.name = name;
+}
+
+public String getFurColor() {
+  return furColor;
+}
+
+public void setFurColor(String furColor) {
+  if (checkPermission()) {
+    this.furColor = furColor;
+  }
+}
+
+public int getAge() {
+  return age;
+}
+
+public void setAge(int age) {
+  this.age = age;
+}
+
+private boolean checkPermission() {
+  return false;
+}
+    
+}
